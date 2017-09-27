67d4dc8778f8c7696587863b435b10e1f059afe1
Unknown <unknown@example.com>
2017-09-26 23:31:48 -0600

    insertar activo nuevo

--- application/libraries/activos_lib.php
+++ application/libraries/activos_lib.php
@@ -13,12 +13,12 @@
  */
 class activos_lib {
     private $CI;
-    private $array=array();
+    private $data_lib=array();
     //put your code here
     
 public function __construct(array $info_data) {
         $this->CI = & get_instance();
-        $this->array=$info_data;
+        $this->data_lib=$info_data;
         $this->CI->load->library('form_validation');
         $this->CI->load->model('activos_model');
     }
@@ -42,20 +42,31 @@ public function __construct(array $info_data) {
         
     }
     public function nuevo_activo(){//insertar activo
-        $this->form_validation->set_rules('name_activo','Nombre del activo','required|trim');
-        $this->form_validation->set_message('required', '%s: es requerido');
-        if ($this->form_validation->run() == FALSE) {
+        $id_activo= $this->data_lib['id_activo'];
+        $this->CI->form_validation->set_rules('name_activo','Nombre del activo','required|trim');
+        $this->CI->form_validation->set_rules('valor_bruto','Valor bruto del activo','required|trim');
+        $this->CI->form_validation->set_rules('fecha_compra','Fecha de compra','required|trim');
+        $this->CI->form_validation->set_message('required', '%s: es requerido');
+        if ($this->CI->form_validation->run() == FALSE) {
             $respuesta=array(
                 'tipo'=>0,
                 'msg'=> validation_errors(),
             );
         }else{
+            $array_data_activo=array(
+                'nombre_activo'=>$_POST['name_activo'],
+                'valor_activo'=>$_POST['valor_activo'],
+                'fecha_compra'=>$_POST['fecha_compra'],
+                
+            );
+            
             $respuesta=array(
                 'tipo'=>1,
                 'msg'=>"correctamente",
+                'activo'=>$id_activo,
             );
         }
-        return json_encode($respuesta);
+        return $respuesta;
     }
     public function calcular_depreciacion() {
         
